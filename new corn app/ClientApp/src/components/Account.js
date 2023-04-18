ea8b56da42bd290f48c8264d1a7ea8f82c727bb2
caswanson <89547763+caswanson@users.noreply.github.com>
2023-04-18 00:39:58 -0500

    table

--- new corn app/ClientApp/src/components/Account.js
+++ new corn app/ClientApp/src/components/Account.js
@@ -1,8 +1,10 @@
 ﻿import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { RealTimeData } from "./realTimeData/index";
 import { useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
 import { database } from '../firebase';
-import { getDatabase, ref, set, child, get, push, update, query } from "firebase/database";
+import { getDatabase, ref, set, child, get, push, update } from "firebase/database";
 import { ChangeEvent, useState } from "react";
 
 const Account = () => {
@@ -12,22 +14,12 @@ const Account = () => {
     // variable storing the inputted new class name
     const [inputText, setInputText] = useState("");
 
-    // variable storing the inputted class code 
-    const [classCodeInput, setClassCodeInput] = useState("");
-
     // creating a new random string of chars to refer to a newly created class
     const newClassKey = push(child(ref(getDatabase()), 'classes')).key;
 
-    // input box to store new class name
     const handleClassInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         // 👇 Store the input value to local state
         setInputText(e.target.value);
-    };
-
-    // input to store class code entered
-    const handleClassCodeInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        // 👇 Store the input value to local state
-        setClassCodeInput(e.target.value);
     };
 
     // logs out a user currently signed in
@@ -50,12 +42,7 @@ const Account = () => {
             students: [],
             admin: [
                 user.uid
-            ],
-            classCode: (Math.floor(Math.random() * 10).toString()
-                + Math.floor(Math.random() * 10).toString()
-                + Math.floor(Math.random() * 10).toString()
-                + Math.floor(Math.random() * 10).toString()
-                + Math.floor(Math.random() * 10).toString())
+            ]
         });
 
 
@@ -63,7 +50,7 @@ const Account = () => {
         const dbRef = ref(getDatabase());
         get(child(dbRef, 'classes/' + newClassKey)).then((snapshot) => {
             if (snapshot.exists()) {
-                console.log(snapshot.val());
+                console.log(snapshot.val().className);
             } else {
                 console.log("No data available");
             }
@@ -72,40 +59,23 @@ const Account = () => {
         });
     };
 
-    // join a already existing class
-    const joinClass = () => {
-        console.log("hi");
-        get(child(ref(getDatabase()), 'classes/')).then((snapshot) => {
-            if (snapshot.exists()) {
-                console.log(snapshot.val());
-                console.log(snapshot.val()[0]);
+    const nameList = []
 
-                snapshot.forEach(function (childSnapshot) {
-                    if (childSnapshot.val().classCode === classCodeInput) {
-                        console.log("YES");
-                        // TODO: add class to user list and update class rosters
-                        //childSnapshot.classesEnrolled
-                    }
-                });
-            } else {
-                console.log("No data available");
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `classes`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            let test = snapshot.val();
+            for (let key in test) {
+                console.log(test[key].className);
+                nameList.push(test[key].className);
+
             }
-        }).catch((error) => {
-            console.error(error);
-        });
-
-    };
-
-    // navigate to the class page
-    const goToClassPage = () => {
-        try {
-            navigate('/class');
-            console.log('went to class')
-        } catch (e) {
-            console.log(e.message);
+        } else {
+            console.log("No data available");
         }
-
-    };
+    }).catch((error) => {
+        console.error(error);
+    });
 
 
     return (
@@ -119,19 +89,12 @@ const Account = () => {
             <button onClick={handleLogout}>Logout</button>
             <br />
             <br />
-
-            
             <input type="text" onChange={handleClassInputChange} value={inputText} />
+
             <button onClick={pushData}>Add class to Firebase</button>
-            <br />
 
-            <input type="text" onChange={handleClassCodeInputChange} value={classCodeInput} />
-            <button onClick={joinClass}>Join Class</button>
-
-            <br />
-            <button onClick={goToClassPage}>Go to the class page!</button>
-
-
+            <h2>List of Firebase Classes</h2>
+            <RealTimeData />
         </div>
     );
 };
---
+++ new corn app/ClientApp/src/components/realTimeData/index.js
@@ -0,0 +1,56 @@
+﻿import React from 'react';
+import { ref as sRef, onValue, getDatabase } from 'firebase/database';
+import { Table } from 'react-bootstrap';
+
+const db = getDatabase();
+
+export class RealTimeData extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            tableData: []
+        }
+    }
+
+
+    componentDidMount() {
+        const dbRef = sRef(db, "classes/");
+
+        onValue(dbRef, (snapshot) => {
+            let records = [];
+            snapshot.forEach(childSnapshot => {
+                let keyName = childSnapshot.key;
+                let data = childSnapshot.val();
+                records.push({ "key": keyName, "data": data });
+            });
+            this.setState({ tableData: records });
+        });
+    }
+
+
+    render() {
+        return (
+            <Table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Class Name</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {this.state.tableData.map((rowdata, index) => {
+                        return (
+                            <tr>
+                                <td>{index}</td>
+                                <td>{rowdata.key}</td>
+                                <td>{rowdata.data.className}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table>
+        )
+    }
+}
+
