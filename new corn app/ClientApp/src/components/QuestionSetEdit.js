8be7780ae31c80a489f5f5a1d6b53cf09efb4d87
mjjfeng <50809301+mjjfeng@users.noreply.github.com>
2023-04-27 11:16:50 -0500

    updated question edit page

--- new corn app/ClientApp/src/components/QuestionSetEdit.js
+++ new corn app/ClientApp/src/components/QuestionSetEdit.js
@@ -14,43 +14,142 @@ const QuestionSetEdit = () => {
     //TODO: question set selector
 
     //question text variable
-    const [questionInput, setQuestionInput] = useState("");
+    const [questionInput, setQuestionInput] = useState(null)
 
     //question type variable
     const [questionType, setQuestionType] = useState(null)
 
     //qType event handler
-    const handleTypeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleTypeChange = (e) => {
         setQuestionType(e.value)
+        //test if changed
+        if (e.value != questionType) {
+            switch (e.value) {
+                case "multi":
+                    console.log("multi-switch")
+                    break;
+                case "TF":
+                    console.log("tf-switch")
+                    break;
+                case "short":
+                    console.log("short-switch")
+                    break;
+            }
+        }
     }
 
-    //variable for qType to handle async nature
-  //  const currentItem = useSelector(state => state.currentItem)
+    const addQuestion = () => {
+
+    }
+
+    //show choices variable
+    const [showChoices, setShowChoices] = useState(true)
+
+    //question text variable
+    const [questionText, setQuestionText] = useState("")
+
+    //handle question text update
+    const handleQTextChange = (e) => {
+        setQuestionText(e.target.value)
+    }
 
     //qType options
     const questionTypeOptions = [
-        { value: "multiChoice", label: "Multiple Choice" },
+        { value: "multi", label: "Multiple Choice" },
         { value: "short", label: "Short Answer" },
         { value: "TF", label: "True/False" }
     ]
 
     //multiple choice variable
+    const [choices, setChoices] = useState([])
+
+    //handle options change
+    const handleOptionChange = (index, event) => {
+        let data = [...choices]
+        data[index][event.target.name] = event.target.value
+        setChoices(data)
+    }
+
+    //add additional multi-choice fields
+    const addFields = () => {
+        let newChoice = { answerChoice: '' }
+
+        setChoices([...choices, newChoice])
+    }
+
+    //remove multi-choice fields
+    const removeFields = (index) => {
+        let data = [...choices]
+        if (data.length > 2) {
+            data.splice(index, 1)
+            setChoices(data)
+        }
+        else {
+            //TODO: Error message, have to have at least 2 options
+        }
+    }
+
+    //debug functions
 
     //console log statement
-    const getVars = () => {
+    const getVars = (e) => {
+        e.preventDefault()
         console.log(questionType)
     }
 
+    //toggle show choices
+    const toggleShowChoice = (e) => {
+        e.preventDefault()
+        setShowChoices(!showChoices)
+        console.log(!showChoices)
+    }
+
+    //TODO: css: make <form> background transparent
+    //TODO: html: make buttons look better
     return (
-        <div>
+        <div className="App">
             <Select
-                isClearable="true"
                 name="Question Type"
                 options={questionTypeOptions}
                 onChange={handleTypeChange}
+                cols={40}
             />
+
             <br />
-            <button onClick={getVars }>log vars</button>
+            <button onClick={getVars}>log vars</button>
+
+            <br />
+            <textarea
+                name="questionText"
+                onChange={handleQTextChange }
+            />
+
+            <br />
+            <div>
+                {choices.map((input, index) => {
+                    return (
+                        <div key={index}>
+                            <input
+                                name='answerChoice'
+                                placeholder='Answer Text'
+                                value={input.answerChoice}
+                                onChange={event => handleOptionChange(index, event)}
+                            />
+                            <button onClick={() => removeFields(index)} >Remove</button>
+                        </div>
+                    )
+                })}
+
+                <br />
+                <select
+                    name="correctAnswer"
+
+
+                />
+
+                <br />
+                <button onClick={addFields}>add field</button>
+            </div>
         </div>
     );
 }
