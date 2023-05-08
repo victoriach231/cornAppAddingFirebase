d1fd90ebcb1b6b640477a76bacbd87b2d6498c71
b-litt <blittman2@huskers.unl.edu>
2023-05-08 17:48:40 -0500

    fixed console warnings, added styling

--- new corn app/ClientApp/src/components/InstructorSessionView.js
+++ new corn app/ClientApp/src/components/InstructorSessionView.js
@@ -269,17 +269,17 @@ const InstructorSessionView = () => {
                 <div class='corner'>
                     <button onClick={backNavigate}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="arrow-back" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                            <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
                         </svg>
 
                     </button>
                 </div>
             </div>
-            <p> hi welcome to instructor view of session </p>
+            <p> Instructor Session View: *class name* </p>
             <br />
 
             <Button variant="primary" onClick={handleStudentBarShow}>
-                See students in the session
+                See Students in Session
             </Button>
 
             <Offcanvas show={showStudentsBar} onHide={handleStudentBarClose}>
@@ -326,16 +326,16 @@ const InstructorSessionView = () => {
                 label="Set timer?"
                 onChange={onSwitchAction}
             />
-            <Button onClick={() => { setNextQuestionIndex(nextQuestionIndex + 1); setAnswerCountMap(new Map()) }}>Display Question</Button>
+            <Button onClick={() => { setNextQuestionIndex(nextQuestionIndex + 1); setAnswerCountMap(new Map()) }}>Display Next Question</Button>
 
+           
+            
             <br />
-            <button onClick={clearQuestionIndexDB}>clear curr question index</button>
-            <br />
-            <Button onClick={getStudentAnswers}>See current student answers</Button>
+            <Button class="btn btn-primary" onClick={getStudentAnswers}>See Current Student Responses</Button>
 
             
 
-            <button onClick={() => { sessionFunctions.download(["a", "b"], "filee") }}>Download CSV</button>
+            <button class="btn btn-primary" onClick={() => { sessionFunctions.download(["a", "b"], "filee") }}>Download CSV</button>
         </div>
     );
 };
